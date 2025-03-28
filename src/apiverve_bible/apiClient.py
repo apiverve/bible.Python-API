157109d638f716b9eeb90a65da0ae631989f7ad1
APIVerve Team <hello@apiverve.com>
2025-03-28 17:01:39 -0500

    Bug Fixes

--- src/apiverve_bible/apiClient.py
+++ src/apiverve_bible/apiClient.py
@@ -10,10 +10,10 @@ class BibleAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/bible'
+        self.base_url = 'https://api.apiverve.com/v1/bible'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
