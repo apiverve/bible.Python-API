96723f64618400a46744b8f9d9f4ad7bb19f2b84
APIVerve Team <hello@apiverve.com>
2025-03-28 17:01:41 -0500

    Bug Fixes

--- src/apiverve_bible/setup.py
+++ src/apiverve_bible/setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirements_file = os.path.join(lib_folder, 'requirements.txt')
+install_requires = ["requests >= 2.25.1", "setuptools >= 56.0.0"]
+if os.path.exists(requirements_file):
+    with open(requirements_file, 'r') as f:
+        install_requires = f.read().splitlines()
+
 setup(
     name='apiverve_bible',
-    version='1.0.9',
+    version='1.1.9',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'requests',
-        'setuptools'
-    ],
+    install_requires=install_requires,
     description='Bible is a simple tool for getting bible verses. Simply provide the book, chapter, and verse, and the API will return the verse.',
     author='APIVerve',
     author_email='hello@apiverve.com',
